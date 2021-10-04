6e4f3eb7ae85f18223be613d14652913f2e445f2
morlandop <91369048+morlandop@users.noreply.github.com>
2021-10-04 18:52:13 +0200

    Update dbConnection.java

--- src/main/java/com/example/airplaneticket/dbConnection.java
+++ src/main/java/com/example/airplaneticket/dbConnection.java
@@ -2,27 +2,40 @@ package com.example.airplaneticket;
 
 import java.sql.*;
 
-public class dbConnection { //receiver
+/**
+        Classe Singleton per la connessione al database
+    */
 
+public class dbConnection { //receiver
+    
+    
+    //Metodo per la connessione al databse
     private dbConnection() throws SQLException, ClassNotFoundException {
         conn = DriverManager.getConnection(DATABASE_URL);
         Class.forName(JDBC_DRIVER);
     }
+    
+    /**
+    Restituisce l'istanza del database se è già istanziato,
+    altrimenti la istanzia e apre una connesione al database
+    */
     public static dbConnection getInstance() throws SQLException, ClassNotFoundException {
         if(dbInstance==null)
             dbInstance = new dbConnection();
         return dbInstance;
     }
-
+    //Metodo per query di tipo inserimento dei dati nel database
     public void insertQuery(String qu) throws SQLException {
         Statement statement = conn.createStatement();
         statement.executeUpdate(qu);
     }
+    //Metodo per query di tipo selezione all'interno del database
     public ResultSet selectQuery(String qu) throws SQLException {
         Statement statement = conn.createStatement();
         ResultSet result = statement.executeQuery(qu);
         return result;
     }
+    //Metodo per query di tipo eliminazione all'interno del database
     public void deleteElement(String qu) throws SQLException {
         Statement statement = conn.createStatement();
         statement.executeUpdate(qu);
