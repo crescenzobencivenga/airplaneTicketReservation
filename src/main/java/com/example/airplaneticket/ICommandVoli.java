1ca3efebbaf1db96a9694f5858c8747c5a1c8350
morlandop <91369048+morlandop@users.noreply.github.com>
2021-10-04 21:21:23 +0200

    Update ICommandVoli.java

--- src/main/java/com/example/airplaneticket/ICommandVoli.java
+++ src/main/java/com/example/airplaneticket/ICommandVoli.java
@@ -5,9 +5,12 @@ import java.sql.SQLException;
 /**
     @author Morlando Pasquale, Bencivenga Crescenzo
     interfaccia command
-* */
+ */
 
 public interface ICommandVoli {
+    /**
+        @param v volo da inserire o eliminare, a seconda del comando eseguito
+    */
     void executeCommand(Volo v) throws SQLException, ClassNotFoundException;
     void undoCommand() throws SQLException, ClassNotFoundException;
 }
