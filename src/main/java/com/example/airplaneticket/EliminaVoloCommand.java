edd0b57ba9d3f80dae219dd84e4199c11baa7f74
crescenzobencivenga <84187113+crescenzobencivenga@users.noreply.github.com>
2021-10-04 21:23:11 +0200

    Update EliminaVoloCommand.java

--- src/main/java/com/example/airplaneticket/EliminaVoloCommand.java
+++ src/main/java/com/example/airplaneticket/EliminaVoloCommand.java
@@ -2,17 +2,23 @@ package com.example.airplaneticket;
 
 import java.sql.SQLException;
 
+
+/** classe che implementa i metodi per l'eliminazione dei voli
+ */
 public class EliminaVoloCommand implements ICommandVoli{
     static Volo ultimoVolo;
 
-
+    /** override execute command per l'eleminazione di un volo
+    * @param volo da eliminare
+    */
     @Override
     public void executeCommand(Volo v) throws SQLException, ClassNotFoundException {
         dbConnection con = dbConnection.getInstance();
         ultimoVolo = v;
         con.deleteElement("DELETE FROM volo WHERE nVolo ="+v.getNVolo());
     }
-
+    /** override undo command per l'eleminazione di un volo
+    */
     @Override
     public void undoCommand() throws SQLException, ClassNotFoundException {
         dbConnection con = dbConnection.getInstance();
