734f712962f10292e4cc5a45806cc00434ae7d4d
morlandop <91369048+morlandop@users.noreply.github.com>
2021-10-04 19:19:38 +0200

    Update IStrategiaPagamento.java

--- src/main/java/com/example/airplaneticket/IStrategiaPagamento.java
+++ src/main/java/com/example/airplaneticket/IStrategiaPagamento.java
@@ -4,6 +4,9 @@ import javafx.event.ActionEvent;
 
 import java.io.IOException;
 
+/**
+    Interfaccia che definisce il metodo per il pagamento
+*/
 public interface IStrategiaPagamento {
     void paga(ActionEvent actionEvent) throws IOException;
 }
