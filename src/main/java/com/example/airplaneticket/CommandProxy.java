0eaab03f1ec770a97c9e6eb10414ef06ce4cd0ea
crescenzobencivenga <84187113+crescenzobencivenga@users.noreply.github.com>
2021-10-04 21:19:38 +0200

    Update CommandProxy.java

--- src/main/java/com/example/airplaneticket/CommandProxy.java
+++ src/main/java/com/example/airplaneticket/CommandProxy.java
@@ -14,20 +14,27 @@ public class CommandProxy implements ICommandVoli {
     }
     
     /** override del metodo executeCommand che esegue l'executeCommand della classe definita 
-    * attraverso il costruttore
+    * attraverso il costruttore e chiama il metodo logging
+    @param volo da inserire o eliminare
     */
     @Override
     public void executeCommand(Volo v) throws SQLException, ClassNotFoundException {
         commandVoli.executeCommand(v);
         addLog(commandVoli.getClass().toString(),"do", v);
     }
-
+    
+    
+    /** override undo command
+    */
     @Override
     public void undoCommand() throws SQLException, ClassNotFoundException {
         commandVoli.undoCommand();
         addLog(commandVoli.getClass().toString(),"undo", null);
     }
 
+    /** metodo per il logging delle operazioni eseguite
+    *   @param classe di appartenenza del command eseguito, il comando eseguito, volo sul quale è eseguito il comando
+    */
     public void addLog(String classe, String comando,Volo v){
         Utente u = Utente.getInstance();
         String log = u.getUsername();
