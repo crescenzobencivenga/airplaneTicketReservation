450f4344936457fa46cb9c9c412dfb0419b0841e
morlandop <91369048+morlandop@users.noreply.github.com>
2021-10-04 21:19:20 +0200

    Update ILoginBuilder.java

--- src/main/java/com/example/airplaneticket/ILoginBuilder.java
+++ src/main/java/com/example/airplaneticket/ILoginBuilder.java
@@ -4,7 +4,9 @@ import javafx.event.ActionEvent;
 
 import java.io.IOException;
 
-/** Costruttore delle due interfacce utente Admin e User
+/**
+    @author Morlando Pasquale, Bencivenga Crescenzo
+    Costruttore delle due interfacce utente Admin e User
  */
 
 public interface ILoginBuilder {
