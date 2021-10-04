17cd6f7ff4cbebbfd67bfcd0be96256324e393c3
morlandop <91369048+morlandop@users.noreply.github.com>
2021-10-04 21:18:56 +0200

    Update IStrategiaPagamento.java

--- src/main/java/com/example/airplaneticket/IStrategiaPagamento.java
+++ src/main/java/com/example/airplaneticket/IStrategiaPagamento.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import java.io.IOException;
 
 /**
+    @author Morlando Pasquale, Bencivenga Crescenzo
     Interfaccia che definisce il metodo per il pagamento
 */
 public interface IStrategiaPagamento {
